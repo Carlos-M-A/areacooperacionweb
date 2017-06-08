1459fcefee9ece2a752965d17b08f3b2c3e1c851
Carlos-M-A <carlos.ma.trabajo@gmail.com>
2017-06-08 21:50:30 +0200

    New lang file

---
+++ resources/lang/en/enums.php
@@ -0,0 +1,57 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Enums Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines contain the names of meanings of enums.
+    | Here there are names of states of classes, types in the application, 
+    | roles, etc. Ergo, the meanings of enums that in the code of application
+    | are represented by a integer.
+    */
+
+    'role_1' => 'Student',
+    'role_2' => 'Teacher',
+    'role_3' => 'Other',
+    'role_4' => 'Organization',
+    'role_5' => 'Cooperation Area',
+    'role_6' => 'Admin',
+    
+    'branch_1' => 'Arts and Humanities',
+    'branch_2' => 'Sciences',
+    'branch_3' => 'Health Sciences',
+    'branch_4' => 'Social and legal sciences',
+    'branch_5' => 'Engineering and Architecture',
+    'branch_6' => 'Other',
+    
+    'proposal_state_1' => 'Not evaluated',
+    'proposal_state_2' => 'Approved',
+    'proposal_state_3' => 'Rejected',
+    'proposal_state_4' => 'Accepted by student',
+    'proposal_state_5' => 'Cancelled',
+    
+    'proposal_type_1' => 'Curricular practices',
+    'proposal_type_2' => 'Just cooperate',
+    
+    'project_state_1' => 'Proposed',
+    'project_state_2' => 'Started',
+    'project_state_3' => 'Finished',
+    
+    'inscription_in_project_state_1' => 'Not chosen',
+    'inscription_in_project_state_2' => 'Chosen',
+    'inscription_in_project_state_3' => 'Cancelled',
+    
+    'convocatory_state_1' => 'Open',
+    'convocatory_state_2' => 'Out of time',
+    'convocatory_state_3' => 'Closed',
+    
+    'inscription_state_1' => 'Not evaluated',
+    'inscription_state_2' => 'Accepted',
+    'inscription_state_3' => 'Alternate',
+    'inscription_state_4' => 'Rejected',
+    'inscription_state_5' => 'Cancelled',
+    
+];
