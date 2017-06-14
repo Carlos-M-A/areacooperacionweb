f884e0043fa8d9dfff35632283c8464b43a4cffd
Carlos-M-A <carlos.ma.trabajo@gmail.com>
2017-06-14 21:54:22 +0200

    Improve the views of projects, offers and convocatories  - Now this views have better GUI

--- app/Campus.php
+++ app/Campus.php
@@ -9,6 +9,7 @@ class Campus extends Model
     protected $table = 'Campus';
     public $timestamps = false;
     
+    
     public function studies() {
         return $this->hasMany('App\Study', 'campus_id', 'id');
     }
